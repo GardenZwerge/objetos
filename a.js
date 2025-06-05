f4e092e5384c5ff18d0829cc0bb2f92d06d550c8
GardenZwerge <87580834+GardenZwerge@users.noreply.github.com>
2025-06-05 11:54:46 +0000

    concluido

---
+++ a.js
@@ -0,0 +1,61 @@
+let Carro ={
+marca: "Toyota",
+cor: "preto",
+ano: 2020
+};
+let Cadeira ={
+material: "madeira",
+cor: "vermelha",
+temEncosto: true
+};
+let Telefone ={
+modelo: "iPhone 13",
+temCamera: true,
+armazenamento: "128GB"
+};
+let Relógio ={
+tipo: "digital",
+marca: "Casio",
+resistenteÁAgua: true
+};
+let Fogão ={
+bocas: 4,
+temForno: true,
+cor: "branco"
+}
+let Ventilador ={
+potencia: "60W",
+velocidades: 3,
+cor: "azul"
+};
+let Mochila ={
+compartimentos: 3,
+cor: "preta",
+temAlmofadaCostas: true
+};
+let Copo ={
+material: "vidro",
+capacidadeML: 300,
+éDescartável: false
+};
+let Chave ={
+tipo: "residencial",
+material: "metal",
+possuiEtiqueta: true
+};
+let Bicicleta ={
+marchas: 21,
+aro: 29,
+cor: "verde"
+};
+console.log(Bicicleta.marchas);
+console.log(Carro.ano);
+console.log(Cadeira.material);
+console.log(Telefone.modelo);
+console.log(Relógio.tipo);
+console.log(Fogão.bocas);
+console.log(Ventilador.potencia);
+console.log(Mochila.comṕartimentos);
+console.log(Copo.material);
+console.log(Chave.tipo);
+console.log(Bicicleta.aro);
